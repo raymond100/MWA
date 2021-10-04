6c44d7805dbabb93856f4d81addf666e075df87a
ray100 <jraymonddoirin@gmail.com>
2021-10-04 18:03:03 -0500

    fixed add  reviews

--- Assignment-05-2/index.js
+++ Assignment-05-2/index.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const path = require('path');
-require("./api/data/db.js")
-const routes = require('./api/routes');
+require("dotenv").config({ path: ".env" });
+const express = require("express");
+const path = require("path");
+require("./api/data/db.js");
+const routes = require("./api/routes");
 
 const app = express();
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 // set port
-app.set('port', PORT);
+app.set("port", PORT);
 
 // body parser
 app.use(express.urlencoded({ extended: false }));
@@ -18,16 +19,13 @@ app.use(express.json());
 //app.use(express.static(path.join(__dirname, 'public')));
 
 // routes middleware
-app.use('/api', routes);
+app.use("/api", routes);
 
-app.get('/', (req, res) => {
-    res.redirect('/api/student');
-})
-
-
-const server = app.listen(app.get('port'), (err) => {
-    if (err) throw err;
-    console.log(`Server running on port ${server.address().port}`);
+app.get("/", (req, res) => {
+  res.redirect("/api/students");
 });
 
-
+const server = app.listen(app.get("port"), (err) => {
+  if (err) throw err;
+  console.log(`Server running on port ${server.address().port}`);
+});
--- Assignment-05-5/api/data/models/games.model.js
+++ Assignment-05-5/api/data/models/games.model.js
@@ -1,38 +1,37 @@
-const mongoose = require('mongoose');
-const { publisherSchema } = require('./publisher.model');
-const { reviewSchema } = require('./review.model');
-const { designerSchema } = require('./designer.model');
+const mongoose = require("mongoose");
+const { publisherSchema } = require("./publisher.model");
+const { reviewSchema } = require("./review.model");
+const { designerSchema } = require("./designer.model");
 
 const gameSchema = mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    year: {
-        type: Number
-    },
-    rate: {
-        type: Number,
-        min: 1,
-        max: 5,
-        default: 1
-    },
-    price: {
-        type: Number
-    },
-    minPlayers: {
-        type: Number
-    },
-    maxPlayers: {
-        type: Number
-    },
-    publisher: publisherSchema,
-    reviews: [reviewSchema],
-    minAge: {
-        type: Number
-    },
-    designers: designerSchema
-
+  title: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+  },
+  rate: {
+    type: Number,
+    min: 1,
+    max: 5,
+    default: 1,
+  },
+  price: {
+    type: Number,
+  },
+  minPlayers: {
+    type: Number,
+  },
+  maxPlayers: {
+    type: Number,
+  },
+  publisher: publisherSchema,
+  reviews: [reviewSchema],
+  minAge: {
+    type: Number,
+  },
+  designers: designerSchema,
 });
 
-mongoose.model('Game', gameSchema, 'games');
+mongoose.model("Game", gameSchema, "games");
