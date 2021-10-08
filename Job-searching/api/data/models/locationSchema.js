39eeeddeceb21fca39b7d790aeb3c40c7903935d
ray100 <jraymonddoirin@gmail.com>
2021-10-08 18:01:18 -0500

    geo loca

--- Job-searching/api/data/models/locationSchema.js
+++ Job-searching/api/data/models/locationSchema.js
@@ -3,8 +3,16 @@ const mongoose = require("mongoose");
 const LocationSchema = mongoose.Schema({
   address: String,
   // Store coordinates in order longitude (E/W), latitude (N/S)
-  coordinates: {
-    type: [Number],
-    index: "2dsphere",
+  location: {
+    type: {
+      type: String, // Don't do `{ location: { type: String } }`
+      enum: ["Point"], // 'location.type' must be 'Point'
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      index: "2dsphere", // Create a special 2dsphere index on `City.location`
+      required: true,
+    },
   },
 });
